5fb2f313c96c7109d8af90a4f85de7a4ebfb9380
D1_Onkar_49801 <onkarbhusare50@gmail.com>
2021-10-17 15:29:28 +0530

    void empty_string_should_return_0() implementation done

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,9 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+        if(input.length() == 0)
+            return 0;
+        return Integer.parseInt(input);
     }
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,20 +1,32 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
+    StringCalculator sc;
+    @BeforeEach
+    void begin()
+    {
+        sc=new StringCalculator();
+    }
+    @Test
+    void empty_string_should_return_zero()
+    {
+        assertEquals(0,sc.add(""));
+    }
 
     @Test
     void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+
+        assertEquals(0, sc.add(""));
     }
 
     @Test
     void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
+
+        assertEquals(1, sc.add("1"));
     }
 }
